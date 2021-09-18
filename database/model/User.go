22cf3e58e07646f27b635c435ff3b13ee3e0f727
KLag-SF <k.lagopus.sf19@gmail.com>
2021-09-18 13:41:52 +0900

    Add CRUD methods

--- database/model/User.go
+++ database/model/User.go
@@ -1,7 +1,9 @@
 package model
 
 import (
+	"ajcgo/database"
 	"github.com/jinzhu/gorm"
+	"github.com/rs/zerolog/log"
 )
 
 type User struct {
@@ -9,5 +11,70 @@ type User struct {
 	Name     string `gorm:"not null"`
 	Email    string `gorm:"unique;not null"`
 	Password string `gorm:"not null"`
-	Messages []Message
+}
+
+func CreateUser(name string, email string, passwd string) {
+	db := database.GetDB()
+	// Create new user
+	user := User{Name: name, Email: email, Password: passwd}
+	if err := db.Create(&user).Error; err != nil {
+		log.Warn().Msgf("%v", err)
+	}
+}
+
+func GetUserById(id int) *User {
+	db := database.GetDB()
+
+	user := User{}
+	if err := db.First(&user, id).Error; err != nil {
+		log.Warn().Msgf("%v", err)
+	}
+
+	return &user
+}
+
+func GetUsersByName(name string) *[]User {
+	db := database.GetDB()
+
+	users := []User{}
+	if err := db.Where("name = ?", name).Find(&users).Error; err != nil {
+		log.Warn().Msgf("%v", err)
+	}
+
+	return &users
+}
+
+func GetUserByEmail(email string) *User {
+	db := database.GetDB()
+
+	user := User{}
+	if err := db.Where("email = ?", email).First(&user).Error; err != nil {
+		log.Warn().Msgf("%v", err)
+	}
+
+	return &user
+}
+
+func UpdateUser(id int, name string, email string, passwd string) {
+	db := database.GetDB()
+
+	user := User{}
+	if err := db.First(&user, id).Error; err != nil {
+		log.Warn().Msgf("%v", err)
+	}
+
+	user.Name = name
+	user.Email = email
+	user.Password = passwd
+	if err := db.Save(&user).Error; err != nil {
+		log.Warn().Msgf("%v", err)
+	}
+}
+
+func DeleteUser(id int) {
+	db := database.GetDB()
+
+	if err := db.Delete(&User{}, id).Error; err != nil {
+		log.Warn().Msgf("%v", err)
+	}
 }
