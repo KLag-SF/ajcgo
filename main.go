68e36c82be95ae90e39e662c4207937afff15307
KLag-SF <k.lagopus.sf19@gmail.com>
2021-09-29 22:34:03 +0900

    a few changes

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"ajcgo/database/model"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
 	"github.com/rs/zerolog/log"
 	"os"
@@ -16,4 +18,19 @@ func main() {
 
 	engine := gin.Default()
 
+	engine.GET("/test", func(context *gin.Context) {
+		context.JSON(200, model.User{
+			Model:    gorm.Model{},
+			Name:     "Test User",
+			Email:    "hello@wor.ld",
+			Password: "password",
+		})
+	})
+
+	if err = engine.Run(":8080"); err != nil {
+		log.Fatal().Msgf(
+			"%v",
+			err,
+		)
+	}
 }
