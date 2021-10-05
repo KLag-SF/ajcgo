dca5835461e28fbb54066b6c41f8431fa94f3628
KLag-SF <k.lagopus.sf19@gmail.com>
2021-10-06 01:04:41 +0900

    update message model

--- database/model/Message.go
+++ database/model/Message.go
@@ -2,40 +2,69 @@ package model
 
 import (
 	"ajcgo/database"
-	"github.com/jinzhu/gorm"
 	"github.com/rs/zerolog/log"
+	"time"
 )
 
 type Message struct {
-	gorm.Model
-	UserId string `gorm:"index:user_id"`
-	Text   string `gorm:"size:140"`
+	Id       string    `gorm:"primary key"`
+	Uid      string    `gorm:"index:uid"`
+	Msg      string    `gorm:"size:140"`
+	PostedAt time.Time `gorm:"type:timestamp"`
 }
 
-func CreateMessage(txt string) {
+func CreateMessage(msg *Message) error {
 	db := database.GetDB()
 
-	m := Message{Text: txt}
-	if err := db.Create(&m).Error; err != nil {
-		log.Warn().Msgf("%v", err)
+	msg.Id = getULID()
+	msg.PostedAt = time.Now()
+
+	if err := db.Create(msg).Error; err != nil {
+		return err
 	}
+
+	return nil
 }
 
-func GetMessage(id int) *Message {
+func GetMessage(id string) *Message {
 	db := database.GetDB()
 
 	msg := Message{}
-	if err := db.First(&msg, id).Error; err != nil {
+	if err := db.Where("id = ?", id).First(&msg).Error; err != nil {
 		log.Warn().Msgf("%v", err)
+		return nil
 	}
 
 	return &msg
 }
 
-func DeleteMessage(id int) {
+func GetMessagesByUserId(uid string) *[]Message {
+	db := database.GetDB()
+
+	var messages []Message
+	if err := db.Where("uid = ?", uid).Find(&messages).Error; err != nil {
+		return nil
+	}
+
+	return &messages
+}
+
+func DeleteMessageById(id string) error {
 	db := database.GetDB()
 
 	if err := db.Delete(&Message{}, id).Error; err != nil {
-		log.Warn().Msgf("%v", err)
+		return err
 	}
+
+	return nil
+}
+
+func DeleteAllMessagesByUserId(uid string) error {
+	db := database.GetDB()
+
+	if err := db.Delete(&Message{}, "uid = ?", uid).Error; err != nil {
+		return err
+	}
+
+	return nil
 }
