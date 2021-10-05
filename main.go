0d2f817571be75fe0620bbc3b28ea3c215dc03cf
KLag-SF <k.lagopus.sf19@gmail.com>
2021-10-05 14:25:43 +0900

    Add user CRUD routing rules

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"ajcgo/app/controller"
 	"ajcgo/database/model"
 	"fmt"
 	"github.com/gin-gonic/gin"
-	"github.com/jinzhu/gorm"
 	"github.com/joho/godotenv"
 	"github.com/rs/zerolog/log"
 	"os"
@@ -19,18 +19,24 @@ func main() {
 	engine := gin.Default()
 
 	engine.GET("/test", func(context *gin.Context) {
-		context.JSON(200, model.User{
-			Model:    gorm.Model{},
-			Name:     "Test User",
-			Email:    "hello@wor.ld",
-			Password: "password",
-		})
+		user := model.User{Name: "kokkun312312", Email: "oguiku"}
+		newUser := model.User{Name: "gami"}
+
+		user.Name = newUser.Name
+		user.Email = newUser.Email
+
+		context.JSON(200, user)
 	})
 
+	engine.POST("/api/user/create", controller.CreateUser)
+
+	engine.GET("/api/user", controller.FindUser)
+
+	engine.PATCH("/api/user/:id", controller.UpdateUser)
+
+	engine.DELETE("/api/user/:id", controller.DeleteUser)
+
 	if err = engine.Run(":8080"); err != nil {
-		log.Fatal().Msgf(
-			"%v",
-			err,
-		)
+		log.Fatal().Msgf("%v", err)
 	}
 }
