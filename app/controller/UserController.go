3593941aae05203f2585bfdec3fd1c0e64899f98
KLag-SF <k.lagopus.sf19@gmail.com>
2021-10-05 14:29:36 +0900

    Add User CRUD

---
+++ app/controller/UserController.go
@@ -0,0 +1,62 @@
+package controller
+
+import (
+	"ajcgo/database/model"
+	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+)
+
+func CreateUser(ctx *gin.Context) {
+	u := model.User{}
+	err := ctx.Bind(&u)
+
+	if err != nil {
+		log.Warn().Msgf("%v", err)
+		ctx.Status(400)
+		return
+	}
+
+	model.CreateUser(u.Name, u.Email, u.Password)
+}
+
+func FindUser(ctx *gin.Context) {
+	user := model.User{}
+
+	if ctx.Query("id") != "" {
+		user = *model.GetUserById(ctx.Query("id"))
+	} else if ctx.Query("email") != "" {
+		user = *model.GetUserByEmail(ctx.Query("email"))
+	} else if ctx.Query("name") != "" {
+		findUsers(ctx)
+	} else {
+		ctx.Status(404)
+		return
+	}
+
+	// Remove password hash
+	user.Password = ""
+	ctx.JSON(200, user)
+}
+
+func UpdateUser(ctx *gin.Context) {
+	postedData := model.User{ID: ctx.Param("id")}
+
+	if err := ctx.Bind(&postedData); err != nil {
+		log.Warn().Msgf("%v", err)
+		return
+	}
+
+	model.UpdateUser(&postedData)
+}
+
+func DeleteUser(ctx *gin.Context) {
+	model.DeleteUser(ctx.Param("id"))
+}
+
+func findUsers(ctx *gin.Context) {
+	users := *model.GetUsersByName(ctx.Query("name"))
+	for _, user := range users {
+		user.Password = ""
+	}
+	ctx.JSON(200, users)
+}
